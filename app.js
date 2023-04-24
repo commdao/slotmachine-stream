9491b8010a8dcbf83cf112895fe0cfe695638eb8
Jon Dao <letstalk@commdao.com>
2023-04-23 23:41:20 -0400

    fixed function and images showing

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-// areas to display selected game info 
 let gameArea = document.getElementById('game-series');
 let practiceArea = document.getElementById('practice-point');
 let characterArea = document.getElementById('character-select');
 let logoArea = document.getElementById('game-logo');
 // button to spin
-let spinBtn = document.getElementById('spin-button');
+let spinBtn = document.getElementById('spin-button'); 
 
 
 const games = [
     {
         name: "Soulcalibur VI",
-        logo: 'imgs\scvi.png',
+        logo: 'imgs/scvi.png',
         type: '3D',
         characters: ["Cassandra", "Astaroth", "Seong-Mina", "Ivy"],
         practice: ["spacing", "guard impact", "interrupt"],
@@ -18,23 +17,15 @@ const games = [
     },
     {
         name: "King of Fighters 2002 Unlimited Match",
-        logo: 'imgs\kof02um.png',
+        logo: 'imgs/kof02um.png',
         type: '2D',
         characters: ["Team Ballbusters", "Team Milifam", "Team Bangs", "Chopskick"],
         practice: ["combos", "hops", "air throw"],
         status: "rollback"
     },
     {
-        name: "Persona 4 Arena Ultimax",
-        logo: 'imgs\p4a.png',
-        type: '2D',
-        characters: ["Elizabeth", "Rise", "Yukari", "Mitsuru"],
-        practice: ["combos", "interrupt", "anti-air"],
-        status: "rollback"
-    },
-    {
         name: "Dead or Alive 5: Last Round",
-        logo: 'imgs\doa5lr.png',
+        logo: 'imgs/doa5lr.png',
         type: '3D',
         characters: ["Tina", "Rachel", "Nyotengu", "Sarah"],
         practice: ["spacing", "holds", "combos"],
@@ -42,7 +33,7 @@ const games = [
     },
     {
         name: "Blazblue: Central Fiction",
-        logo: 'imgs\BlazBlue_Centralfiction_(Logo).png',
+        logo: 'imgs/BlazBlue_Centralfiction_(Logo).png',
         type: '2D',
         characters: ["Amane", "Platinum", "Litchi", "Nine"],
         practice: ["combos", "interrupt"],
@@ -50,7 +41,7 @@ const games = [
     },
     {
         name: "Million Arthur: Arcana Blood",
-        logo: 'imgs\MAAB.png',
+        logo: 'imgs/MAAB.png',
         type: '2D',
         characters: ["Eternal Flame", "Bisclavret"],
         practice: ["combos", "anti-air"],
@@ -58,7 +49,7 @@ const games = [
     },
     {
         name: "Under Night In-Birth Exe Late[cl-r]",
-        logo: 'imgs\Uniclr-logo.png',
+        logo: 'imgs/Uniclr-logo.png',
         type: '2D',
         characters: ["Phonon", "Mika", "Eltnum"],
         practice: ["combos", "interrupt"],
@@ -66,7 +57,7 @@ const games = [
     },
     {
         name: "Guilty Gear XX Accent Core Plus R",
-        logo: 'imgs\Guilty_Gear_XX_Accent_Core_Plus_R_Logo.png',
+        logo: 'imgs/Guilty_Gear_XX_Accent_Core_Plus_R_Logo.png',
         type: '2D',
         characters: ["Jam"],
         practice: ["combos", "interrupt"],
@@ -74,34 +65,26 @@ const games = [
     }
 ]
 
-// const games = {
-//     5: {
-//         games: ["SCVI", "P4AU", "KOF02UM", "DOA5LR", "MAAB"]
-//     },
-//     4: {
-//         games: ["UNICLR", "GGACPR", "BBCF"]
-//     },
-//     3: {
-//         games: ["Tekken", "GGST", "Nitroplus"]
-//     },
-//     2: {
-//         games: ["PRBFTG", "MvCi"]
-//     },
-//     1: {
-//         games: ["TMNT x JL", "Granblue", "Arcana Heart 3"]
-//     },
-//     0: {
-//         games: ["SFV", "Killer Instinct"]
-//     }
-// }
-
-
-// Randomizer to pick game and stipulations
-// THIS IS NOT WORKING CORRECTLY, not being collective, being separate randoms
 spinBtn.addEventListener('click', () => {
-        gameArea.innerHTML = games[Math.floor(Math.random() * 5) + 1].logo;
-        practiceArea.innerHTML = games[Math.floor(Math.random() * 5) +1].practice[Math.floor(Math.random() * 2)];
-});
+    // Clear out previous results
+    gameArea.innerHTML = '';
+    practiceArea.innerHTML = '';
+    // Generate random game and practice point
+    let game = games[Math.floor(Math.random() * games.length)];
+    let practicePoint = game.practice[Math.floor(Math.random() * game.practice.length)];
+    // Generate random animation durations for the spin
+    let gameDuration = Math.floor(Math.random() * 0 + 1) * 1000; // Between 3 and 6 seconds
+    let practiceDuration = Math.floor(Math.random() * 0 + 1) * 1000; // Between 3 and 6 seconds
+    // Set the HTML for the spinning animation
+    gameArea.innerHTML = '<div class="spinner-border text-primary" role="status"><span class="sr-only">Loading...</span></div>';
+    practiceArea.innerHTML = '<div class="spinner-border text-secondary" role="status"><span class="sr-only">Loading...</span></div>';
+    // Delay the reveal of the results to simulate spinning
+    setTimeout(() => {
+      // Set the HTML for the results
+      gameArea.innerHTML = `<img src="${game.logo}" alt="${game.name}">`;
+      practiceArea.innerHTML = `<h3>${practicePoint}</h3>`;
+    }, Math.max(gameDuration, practiceDuration));
+  });
 
 
 // display explanation of rules
